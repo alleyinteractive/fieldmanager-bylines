c5cfe0bba4dd0a04860bf6de2a488cf69ef8bd7b
Erick Hitter <git-contrib@ethitter.com>
2018-02-24 15:27:09 -0800

    Load Fieldmanager for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,6 +23,12 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
+	$_fm_dir = getenv( 'FM_DIR' );
+	if ( empty( $_fm_dir ) ) {
+		$_fm_dir = dirname( __FILE__ ) . '/../../wordpress-fieldmanager';
+	}
+	require $_fm_dir . '/fieldmanager.php';
+
 	require dirname( dirname( __FILE__ ) ) . '/fieldmanager-bylines.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
