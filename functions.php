3a203f7689ad958d01809ce02b129ffbe16f0ca2
Erick Hitter <git-contrib@ethitter.com>
2018-02-23 19:54:27 -0800

    PHPCS

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
 <?php
 /**
+ * Plugin functions
+ *
+ * @package Fieldmanager_Bylines
+ */
+
+/**
  * Get a list of byline ids for a post
  *
- * @param int    $post_id
- * @param string $type. Defaults to author.
+ * @param int    $post_id Post ID.
+ * @param string $type Defaults to author.
  * @return array
  */
 function fm_get_byline_ids( $post_id = null, $type = 'author' ) {
@@ -16,9 +22,9 @@ function fm_get_byline_ids( $post_id = null, $type = 'author' ) {
 /**
  * Get a list of byline objects for a post
  *
- * @param int    $post_id
- * @param string $type. Defaults to author.
- * @param array  $args.
+ * @param int    $post_id Post ID.
+ * @param string $type Defaults to author.
+ * @param array  $args Byline arguments.
  * @return string
  */
 function fm_get_byline( $post_id = null, $type = 'author', $args = array() ) {
@@ -31,9 +37,9 @@ function fm_get_byline( $post_id = null, $type = 'author', $args = array() ) {
 /**
  * Get the posts for a byline user
  *
- * @param int    $byline_id.  The byline guest user id.
- * @param string $type.
- * @param array  $args.
+ * @param int    $byline_id The byline guest user id.
+ * @param string $type Byline type.
+ * @param array  $args Byline arguments.
  * @return array.
  */
 function fm_get_byline_posts( $byline_id, $type = 'author', $args = array() ) {
@@ -43,9 +49,9 @@ function fm_get_byline_posts( $byline_id, $type = 'author', $args = array() ) {
 /**
  * Get the WP Posts Query for a byline user.
  *
- * @param int    $byline_id. Defaults to current displayed byline id.
- * @param string $type. Defaults to current display byline type.
- * @param array  $args.
+ * @param int    $byline_id Defaults to current displayed byline id.
+ * @param string $type Defaults to current display byline type.
+ * @param array  $args Byline arguments.
  * @return mixed. WP Query object.
  */
 function fm_get_byline_posts_query( $byline_id = null, $type = null, $args = array() ) {
@@ -55,8 +61,8 @@ function fm_get_byline_posts_query( $byline_id = null, $type = null, $args = arr
 /**
  * Get the html byline url for all bylines of a given type for a single post
  *
- * @param int, post_id
- * @param string. type
+ * @param int    $post_id Post ID.
+ * @param string $type Byline type.
  * @return string
  */
 function fm_get_bylines_posts_links( $post_id = null, $type = 'author' ) {
@@ -70,7 +76,8 @@ function fm_get_bylines_posts_links( $post_id = null, $type = 'author' ) {
  * Get the html markup for a single byline link
  * Takes a single byline id
  *
- * @param int. byline_id
+ * @param int    $byline_id Byline ID.
+ * @param string $type Byline type.
  * @return string
  */
 function fm_get_byline_link( $byline_id, $type = 'author' ) {
@@ -81,32 +88,33 @@ function fm_get_byline_link( $byline_id, $type = 'author' ) {
  * Get the byline url
  * Takes a single byline id
  *
- * @param int. byline_id
+ * @param int    $byline_id Byline ID.
+ * @param string $type Byline type.
  * @return string
  */
 function fm_get_byline_url( $byline_id = null, $type = 'author' ) {
 	if ( ! empty( $byline_id ) ) {
 		return FM_Bylines()->get_byline_url( $byline_id, $type );
 	}
-	return;
+	return '';
 }
 
 /**
  * Add in a FM_Byline meta box w/ all it's bells and whistles
  *
- * @param string $type
- * @param string. Optional label
- * @param array
+ * @param string $type Byline type.
+ * @param string $label Optional label.
+ * @param array  $args Byline arguments.
  */
 function fm_add_byline_meta_box( $type = 'author', $label = null, $args = array() ) {
-	return FM_Bylines()->add_byline_meta_box( $type, $label, $args );
+	FM_Bylines()->add_byline_meta_box( $type, $label, $args );
 }
 
 /**
  * Check if we are on a byline archive page.  Equivalent of is_author().
  *
- * @param string $type
- * @param int. byline id
+ * @param string $type Byline type.
+ * @param int    $byline_id Byline ID.
  * @return boolean
  */
 function fm_is_byline( $type = 'author', $byline_id = null ) {
@@ -116,12 +124,12 @@ function fm_is_byline( $type = 'author', $byline_id = null ) {
 /**
  * Get Byline meta data.  Equivalent of get_the_author_meta().
  *
- * @param string $field
- * @param int    $byline id
+ * @param string $field Byline meta field.
+ * @param int    $byline_id Byline ID.
  * @return mixed
  */
 function fm_get_the_byline_meta( $field, $byline_id ) {
-	if ( in_array( $field, array( 'about', 'contact_info', 'names' ) ) ) {
+	if ( in_array( $field, array( 'about', 'contact_info', 'names' ), true ) ) {
 		$meta_field = 'fm_bylines_' . $field;
 	} else {
 		$meta_field = $field;
@@ -129,6 +137,14 @@ function fm_get_the_byline_meta( $field, $byline_id ) {
 	return FM_Bylines()->get_the_byline_meta( $meta_field, $byline_id );
 }
 
+/**
+ * Get Byline avatar
+ *
+ * @param int   $byline_id Byline ID.
+ * @param mixed $size Avatar size.
+ * @param array $args Byline arguments.
+ * @return string
+ */
 function fm_get_byline_avatar( $byline_id, $size, $args ) {
 	return FM_Bylines()->get_byline_avatar( $byline_id, $size, $args );
 }
@@ -137,8 +153,8 @@ function fm_get_byline_avatar( $byline_id, $size, $args ) {
  * This function exists to attempt to create a label from a slug.
  * If the label is complex you can use the filter hook to create a label lookup or modify single slugs.
  *
- * @param string $slug
- * @param string $callback Callback function to perform on wordified slug
+ * @param string $slug Slug to convert.
+ * @param string $callback Callback function to perform on wordified slug.
  * @return string
  */
 function fm_bylines_wordify_slug( $slug, $callback = 'ucwords' ) {
